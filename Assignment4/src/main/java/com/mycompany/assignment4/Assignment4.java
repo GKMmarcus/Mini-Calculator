373b07d4d9ea0cd5a3aa12ff1d1748e2c5b08fd8
Marcus <152306593+GKMmarcus@users.noreply.github.com>
2024-04-28 16:49:19 -0400

    Add files via upload

---
+++ Assignment4/src/main/java/com/mycompany/assignment4/Assignment4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment4;
+
+/**
+ *
+ * @author marcu
+ */
+public class Assignment4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
